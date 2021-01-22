9ab0bb2d327a3acab4dce369f8033a8a406c3fc1
Sourav Santosh Patil <67873791+sourav1859@users.noreply.github.com>
2021-01-22 19:52:27 +0530

    Add files via upload

---
+++ server_log_admin.php
@@ -0,0 +1,52 @@
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+session_start();
+$link = mysqli_connect("localhost", "root", "", "dbms_project");
+ 
+ $id ='';
+ $pass ='';
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Escape user inputs for security
+if(isset($_POST['user'])){
+	$id = mysqli_real_escape_string($link, $_REQUEST['user']);
+	$pass = mysqli_real_escape_string($link, $_REQUEST['pass']);
+}
+
+$query = "SELECT * FROM admin WHERE ID=? and Password=?";
+ 
+ $stmt = $link->prepare($query);
+     $stmt->bind_param("ss", $id,$pass);
+     $stmt->execute();
+     $stmt->bind_result($id,$pass);
+     $stmt->store_result();
+     $count = $stmt->num_rows;
+
+if ($count == 1){
+//echo "Login Credentials verified";
+	$_SESSION['ctemp'] = $count;
+echo "<script type='text/javascript'>alert('Login Credentials verified')
+</script>";
+    $_SESSION['ADID'] = $id;
+    $_SESSION["login_flag"] = 3;
+    $_SESSION["login_admin"] = true;
+    $_SESSION["login_auth"] = false;
+    $_SESSION["login"] = false;
+    header("location: /projects/admin/admin_page1.php");
+
+//echo '<script> window.location ="Register.php"';
+   
+}else{
+echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+//echo "Invalid Login Credentials";
+}
+$stmt->close();
+
+// Close connection
+mysqli_close($link);
+?>
